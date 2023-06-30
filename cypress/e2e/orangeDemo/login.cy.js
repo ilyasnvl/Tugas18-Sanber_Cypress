76109f9382317fb4dd2b3c5f13b6203f24499eba
ilyasnvl <ilyasnauval96@gmail.com>
2023-06-30 15:13:35 +0700

    update file

--- cypress/e2e/orangeDemo/login.cy.js
+++ cypress/e2e/orangeDemo/login.cy.js
@@ -1,4 +1,5 @@
 import elemLogin from "../../pageObject/elemLogin"
+const inputData = require("../../fixtures/orangeHrm/data.json")
 
 describe("Login", () => {
     const elLogin = new elemLogin
@@ -8,28 +9,25 @@ describe("Login", () => {
         it("Successfully Login", () => {
             elLogin.inputUsername()
             elLogin.inputPassword()
-            cy.get(elLogin.btnLogin).click()
+            elLogin.clickLogin()
             cy.url().should("eq", "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
         })
         it("Failed Login with wrong username", () => {
-            cy.get("[name='username']").type("usersalah")
-            cy.get("[name='password']").type("admin123")
-            cy.get(".oxd-button").click()
-            cy.get(".oxd-alert").should("be.visible")
-            cy.get(".oxd-alert").should("contain.text", "Invalid credentials")
+            elLogin.inputWrongUsername(inputData.invalidUser) // ngambil data dari fixture json
+            elLogin.inputPassword()
+            elLogin.clickLogin()
+            elLogin.tickerInvalidLogin()
+            elLogin.tickerInvalidLogin()
         })
         it("Failed Login with wrong password", () => {
-            cy.get("[name='username']").type("Admin")
-            cy.get("[name='password']").type("passwordsalah")
-            cy.get(".oxd-button").click()
-            cy.get(".oxd-alert").should("be.visible")
-            cy.get(".oxd-alert").should("contain.text", "Invalid credentials")
+            elLogin.inputUsername()
+            elLogin.inputWrongPassword(inputData.invalidPass) // ngambil data dari fixture json
+            elLogin.clickLogin()
+            elLogin.tickerInvalidLogin()
         })
         it("Failed Login without input username & password", () => {
-            cy.get(".oxd-button").click()
-            cy.get(".oxd-form > div:nth-of-type(1) .oxd-text").should("be.visible")
-            cy.get(".oxd-form > div:nth-of-type(1) .oxd-text").should("contain.text", "Required")
-            cy.get(".oxd-form > div:nth-of-type(2) .oxd-text").should("be.visible")
-            cy.get(".oxd-form > div:nth-of-type(2) .oxd-text").should("contain.text", "Required")
+            elLogin.clickLogin()
+            elLogin.errMsgUsrname()
+            elLogin.errMsgPassw()
         })
 })
--- cypress/pageObject/elemLogin.js
+++ cypress/pageObject/elemLogin.js
@@ -2,12 +2,39 @@ class loginElem{
     username = "[name='username']"
     password = "[name='password']"
     btnLogin = ".oxd-button"
+    tickerInvalidCredentials = ".oxd-alert"
+    errMsgUsername = ".oxd-form > div:nth-of-type(1) .oxd-text"
+    errMsgPassword = ".oxd-form > div:nth-of-type(2) .oxd-text"
 
     inputUsername(){
         cy.get(this.username).type("Admin")
     }
     inputPassword(){
         cy.get(this.password).type("admin123")
+    }
+    clickLogin(){
+        cy.get(this.btnLogin).click()
+    }
+    inputWrongUsername(user){
+        cy.get(this.username).type(user)
+    }
+    inputWrongPassword(password){
+        cy.get(this.password).type(password)
+    }
+    tickerInvalidLogin(){
+        cy.get(this.tickerInvalidCredentials)
+        .should("be.visible")
+        .should("contain.text", "Invalid credentials")
+    }
+    errMsgUsrname(){
+        cy.get(this.errMsgUsername)
+        .should("be.visible")
+        .should("contain.text", "Required")
+    }
+    errMsgPassw(){
+        cy.get(this.errMsgPassword)
+        .should("be.visible")
+        .should("contain.text", "Required")
     }
 }
 
